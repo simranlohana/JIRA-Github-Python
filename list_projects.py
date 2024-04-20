482dd977235180761ffd18a9f73370be772feb6f
simranlohana <97595115+simranlohana@users.noreply.github.com>
2024-04-20 21:45:43 +0530

    Update list_projects.py

--- list_projects.py
+++ list_projects.py
@@ -1,14 +1,15 @@
 # This code sample uses the 'requests' library:
 # http://docs.python-requests.org
 import requests
-from requests.auth import HTTPBasicAuth
+from requests.auth import HTTPBasicAuth 
 import json
 
-url = "https://veeramallaabhishek.atlassian.net/rest/api/3/project"
+url = "https://simranlohana7.atlassian.net/rest/api/3/project"
 
-API_TOKEN=""
+API_TOKEN = "ATATT3xFfGF03AllxxqXQYmbULxYfgsQ79D3-n9ZY9plcBwvdXlkEUIDmi1QtOY0f6TYSv5nW-uZ3r9aiqBgjBobqYYXyB7qxrhuuybvoc88ISI_qCVn9464U_bBnQOi6L7OKAPbVpnlhqX7adjBVgQkZ_KHKLDOba4-a_u2oMtoQioE8cRyIzs=054702FA"
 
-auth = HTTPBasicAuth("", API_TOKEN)
+
+auth = HTTPBasicAuth("simranlohana7@gmail.com", API_TOKEN)
 
 headers = {
   "Accept": "application/json"
